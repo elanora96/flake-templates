326f1eb27933df30b591eb14f92e1e9be245876b
Elanora Manson <git@elanora.lol>
2025-04-27 11:06:54 -0700

    Add Rust Template

--- flake.nix
+++ flake.nix
@@ -50,9 +50,24 @@
           description = "A basic Nodejs Template";
           buildTools = [
             "Nodejs"
+            "buildNpmPackage"
+            "importNpmLock"
           ];
           additionalSetupInfo = [
             "run npm init or similar project creation script"
+          ];
+        };
+        rust = mkWelcomeText {
+          path = ./rust;
+          name = "Rust Template";
+          description = "A basic Rust Template";
+          buildTools = [
+            "Standard Rust tools"
+            "Crane"
+            "Rust Flake"
+          ];
+          additionalSetupInfo = [
+            "run cargo new or similar project creation script"
           ];
         };
       };
